e5ac8291eb289ee5dbcfe59e0a95a652206384a8
chasedurrett <chase.durrett80@gmail.com>
2020-05-18 18:29:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, {Component} from 'react';
-import Form from './Components/Form';
-import VehicleDisplay from './Components/VehicleDisplay';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./Components/Form";
+import VehicleDisplay from "./Components/VehicleDisplay";
+import "./App.css";
+import axios from "axios";
 
-class App extends Component  {
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      vehicles: []
-    }
+      vehicles: [],
+    };
+    this.addVehicle = this.addVehicle.bind(this);
   }
 
-  render(){
-    console.log(this.state.vehicles)
+  componentDidMount() {
+    axios
+      .get("https://joes-autos.herokuapp.com/api/vehicles")
+      .then((res) => {
+        this.setState({
+          vehicles: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  addVehicle(newCar) {
+    axios
+      .put("https://joes-autos.herokuapp.com/api/vehicles", newCar)
+      .then((res) => {
+        this.setState({ vehicles: res.data });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    console.log(this.state.vehicles);
     const mappedVehicles = this.state.vehicles.map((vehicle, i) => (
-      <VehicleDisplay key={i} vehicle={vehicle}/>
-    ))
+      <VehicleDisplay key={i} vehicle={vehicle} />
+    ));
     return (
       <div className="App">
         WR1 HTTP/Axios Review
-        <Form />
+        <Form addVehicle={this.addVehicle} />
         {mappedVehicles}
       </div>
-    )
+    );
   }
 }
 
